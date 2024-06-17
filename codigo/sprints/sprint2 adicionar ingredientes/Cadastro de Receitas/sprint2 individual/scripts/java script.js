1fb66c48b74677a01ae70a41a17cb11b66a7bf5d
Gabriel Diniz <gabrieldrvianna@gmail.com>
2024-06-17 14:28:12 -0300

    update

--- codigo/sprints/sprint2 adicionar ingredientes/Cadastro de Receitas/sprint2 individual/scripts/java script.js
+++ codigo/sprints/sprint2 adicionar ingredientes/Cadastro de Receitas/sprint2 individual/scripts/java script.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const formulario = document.querySelector(".main-content");
 const ingredientes = document.querySelector(".ingredientes");
 
@@ -29,6 +30,38 @@ ingredientes.addEventListener("click", function(event) {
         event.target.parentNode.remove();
       }
   }
+=======
+const formulario = document.querySelector(".main-content");
+const ingredientes = document.querySelector(".ingredientes");
+
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const input = formulario.querySelector("input");
+
+  if (input.value.trim() !== '') {
+    addIngrediente(input.value);
+    input.value = "";
+  }
+});
+
+function addIngrediente(nomeIngrediente) {
+
+    ingredientes.innerHTML += `
+    <div class="ingrediente">
+        <p>${nomeIngrediente}</p>
+            <i class="ph ph-trash lixo"></i>
+    </div>
+  `
+}
+
+ingredientes.addEventListener("click", function(event) {
+  if(event.target.classList.contains("lixo")) {
+    if (confirm("Deseja deletar o item?")) {
+        event.target.parentNode.remove();
+      }
+  }
+>>>>>>> 62cf68996ebd04bce730bb9c241df358a9220108
 =======
 const formulario = document.querySelector(".main-content");
 const ingredientes = document.querySelector(".ingredientes");
