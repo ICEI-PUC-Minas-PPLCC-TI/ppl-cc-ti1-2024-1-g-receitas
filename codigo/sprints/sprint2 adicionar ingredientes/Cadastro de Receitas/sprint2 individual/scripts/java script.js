4b713f17ab02ae73e922cde76482a3e3b5562d8c
Gabriel Diniz <gabrieldrvianna@gmail.com>
2024-06-17 14:32:16 -0300

    up

--- codigo/sprints/sprint2 adicionar ingredientes/Cadastro de Receitas/sprint2 individual/scripts/java script.js
+++ codigo/sprints/sprint2 adicionar ingredientes/Cadastro de Receitas/sprint2 individual/scripts/java script.js
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const formulario = document.querySelector(".main-content");
 const ingredientes = document.querySelector(".ingredientes");
 
@@ -127,4 +128,36 @@ ingredientes.addEventListener("click", function(event) {
       }
   }
 >>>>>>> 62cf68996ebd04bce730bb9c241df358a9220108
+=======
+const formulario = document.querySelector(".main-content");
+const ingredientes = document.querySelector(".ingredientes");
+
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const input = formulario.querySelector("input");
+
+  if (input.value.trim() !== '') {
+    addIngrediente(input.value);
+    input.value = "";
+  }
+});
+
+function addIngrediente(nomeIngrediente) {
+
+    ingredientes.innerHTML += `
+    <div class="ingrediente">
+        <p>${nomeIngrediente}</p>
+            <i class="ph ph-trash lixo"></i>
+    </div>
+  `
+}
+
+ingredientes.addEventListener("click", function(event) {
+  if(event.target.classList.contains("lixo")) {
+    if (confirm("Deseja deletar o item?")) {
+        event.target.parentNode.remove();
+      }
+  }
+>>>>>>> 2a089550246db46a31f1e70d8fa64bb32f05e3ae
 });
