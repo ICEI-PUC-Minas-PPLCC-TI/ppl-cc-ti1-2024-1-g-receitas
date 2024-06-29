08f714dc364ff4b628fb29ac749c4ad1fd936bb8
Gabriel Diniz <gabrieldrvianna@gmail.com>
2024-06-29 16:11:23 -0300

    update

--- codigo/pagina-principal/assets/js/pagPrincipal.js
+++ codigo/pagina-principal/assets/js/pagPrincipal.js
@@ -1,30 +1,3 @@
-/*const url = "https://d2c501fa-4177-4d7b-a69b-81eb77e71b05-00-1wjvhqjrblfl5.kirk.replit.dev/receitas?_limit=3"
-const carousel = document.querySelector(".carousel-inner")
-
-async function carregarReceitas()
-{
-    const response = await fetch(url)
-    const data = await response.json()
-
-    data.forEach(receita => {
-        carousel.innerHTML += `
-            <div class="carousel-item">
-                <img src="${receita.imagem}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>${receita.nome}</h5>
-                </div>
-            </div>
-        `
-    })
-
-    carousel.children[0].classList.add("active")
-
-}
-
-window.onload = carregarReceitas*/
-
-
-
 const url = "https://d2c501fa-4177-4d7b-a69b-81eb77e71b05-00-1wjvhqjrblfl5.kirk.replit.dev/receitas?_limit=5"
 const carousel = document.querySelector(".carousel-inner")
 let slideInterval;
