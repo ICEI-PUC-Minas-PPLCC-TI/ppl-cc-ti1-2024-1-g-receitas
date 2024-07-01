4d803d8f581a62b5076799a41c121264bd5dfe9b
Augusto Bizarria <165205881+augustbiza@users.noreply.github.com>
2024-06-30 21:20:53 -0300

    update

--- codigo/pagina-perfil/perfil/assets/js/editPerfil.js
+++ codigo/pagina-perfil/perfil/assets/js/editPerfil.js
@@ -1,8 +1,14 @@
+const lvlIpt = document.querySelector('#lvl');
 const nomeIpt = document.querySelector("#nome")
+const loginIpt = document.querySelector("#login")
+const emailIpt = document.querySelector("#email")
 const bioIpt = document.querySelector("#bio")
 const saveBtn = document.querySelector(".save")
 
+const lvl = document.querySelector(".LVL")
 const nome = document.querySelector(".nome")
+const login = document.querySelector(".login")
+const email = document.querySelector(".email")
 const bio = document.querySelector(".bio")
 
 function carregarPerfil() {
@@ -10,6 +16,8 @@ function carregarPerfil() {
         return res.json()
     }).then((data) => {
         nome.textContent = data.nome
+        login.textContent = data.login
+        email.textContent = data.email
         bio.textContent = data.bio
     })
 }
@@ -18,7 +26,7 @@ window.onload = carregarPerfil
 
 saveBtn.onclick = async () => {
     console.log("akdhsakjd");
-    if(nomeIpt.value.trim() === "" || bioIpt.value.trim() === "") {
+    if(nomeIpt.value.trim() === "" || loginIpt.value.trim() === "" || emailIpt.value.trim() === "" || bioIpt.value.trim() === "") {
         return;
     }
 
@@ -29,6 +37,8 @@ saveBtn.onclick = async () => {
         method: "PATCH",
         body: JSON.stringify({
             nome: nomeIpt.value,
+            login: loginIpt.value,
+            email: emailIpt.value,
             bio: bioIpt.value
         }),
     }).then(() => {
@@ -37,3 +47,14 @@ saveBtn.onclick = async () => {
         alert(err)
     })
 }
+
+
+fetch("https://d2c501fa-4177-4d7b-a69b-81eb77e71b05-00-1wjvhqjrblfl5.kirk.replit.dev/feitos")
+    .then(res => res.json())
+    .then(feitos => {
+        let nivel = Math.floor(feitos.length / 3) + 1;
+        lvl.textContent = `LVL ${nivel}`;
+    })
+    .catch(err => {
+        console.error('Erro ao carregar feitos: ', err);
+    });
